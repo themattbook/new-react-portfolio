f14685d36173db9a90dd4403b05b95478752e4b5
Matthew Sweet <themattbook@gmail.com>
2023-12-26 16:09:23 -0600

    Home UI and content

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,90 @@
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
+import { FaFileDownload } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaEnvelope } from "react-icons/fa";
+import { useState } from "react";
+import Me from "./assets/me.webp";
+
 function App() {
-  return (
-    <div className='flex flex-col min-h-screen bg-slate-950 text-gray-200'>
-      <div className="container mx-auto">
-      <Navbar />
-      <div className='flex items-center h-[80vh] p-4'>
-        <div className="w-1/2">
+	const [darkMode, setDarkMode] = useState(true);
+	const toggleDarkMode = () => {
+		setDarkMode((prevDarkMode) => !prevDarkMode);
+	};
 
-      <h1>Hello, world!</h1>
-      <p className="text-sm">My name is Matt Sweet and I&apos;m a seasoned FullStack 
-      developer with professional experience on a wide array of projects. I prefer working 
-      in the frontend, but I am not afraid of the backend. My most recent role was a Frontend
-      Developer at Digital Freight Network.</p>
-      <button className="accent hover:text-emerald-300 font-bold py-4 text-xs">Download my resume</button>
-        </div>
-      </div>
-    </div>
-
-      </div>
-  )
+	return (
+		<div
+			className={`flex flex-col min-h-screen transition-colors ${
+				darkMode
+					? "dark bg-stone-950 text-gray-200"
+					: "bg-white text-gray-800"
+			}`}
+		>
+			<div className="w-1/2 mx-auto">
+				<Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+				<div className="flex p-4 mt-24">
+					<div className="w-1/2">
+						<h1>Hello, world! 👋</h1>
+						<p className="text-lg mt-3">
+							I&apos;m Matt and I&apos;m a seasoned FullStack
+							developer with professional experience on a wide
+							array of projects. I prefer working in the frontend,
+							but I am not afraid of the backend. My most recent
+							role was a Frontend Developer at Digital Freight
+							Network.
+						</p>
+						<p className="text-lg mt-3">Want to know more?</p>
+						<ul className="font-bold mt-3 text-sm">
+							<li className="mt-5">
+								<a
+									className="flex items-center"
+									download
+									href="./assets/Matthew_Sweet_Resume.docx"
+								>
+									<FaFileDownload className="mr-3" /> My
+									Resume (.docx, 583kb)
+								</a>
+							</li>
+							<li className="mt-5">
+								<a
+									className="flex items-center"
+									href="https://github.com/themattbook"
+									target="_blank"
+								>
+									<FaGithub className="mr-3" /> My Github
+								</a>
+							</li>
+							<li className="mt-5">
+								<a
+									className="flex items-center"
+									href="https://linkedin.com/in/meetmattsweet"
+									target="_blank"
+								>
+									<FaLinkedin className="mr-3" /> My LinkedIn
+								</a>
+							</li>
+							<li className="mt-5">
+								<a
+									className="flex items-center"
+									href="mailto:themattbook@gmail.com"
+									target="_blank"
+								>
+									<FaEnvelope className="mr-3" /> Email
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div className="w-1/2 flex flex-col items-center">
+						<img
+							src={Me}
+							alt="An artist's impression of me"
+							className="h-96 rounded ml-auto"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,26 @@
-function Navbar() {
-    return (
-            <nav className="flex w-full p-4">
-                <ul className="flex ml-auto text-xs text-gray-50 font-bold uppercase">
-                    <li className="mr-8">Home</li>
-                    <li className="mr-8">Projects</li>
-                    <li className="mr-8">Contact</li>
-                    <li>More</li>
-                </ul>
-            </nav>
-    )
+import { FaSun } from "react-icons/fa";
+import { HiMiniMoon } from "react-icons/hi2";
+
+interface NavbarProps {
+	darkMode: boolean;
+	toggleDarkMode: () => void;
 }
 
-export default Navbar
+function Navbar(props: NavbarProps) {
+	return (
+		<nav className="flex items-center w-full p-4">
+			<h4>Matt Sweet</h4>
+			<ul className="flex items-center ml-auto text-sm text-gray-800 dark:text-gray-200 font-bold uppercase">
+				<li className="mr-5">Home</li>
+				<li className="mr-5">Projects</li>
+				<li className="mr-5">Contact</li>
+				<li className="mr-8">More</li>
+				<li className="text-2xl" onClick={props.toggleDarkMode}>
+					{props.darkMode ? <FaSun /> : <HiMiniMoon />}
+				</li>
+			</ul>
+		</nav>
+	);
+}
+
+export default Navbar;
